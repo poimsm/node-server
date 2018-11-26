8c739e906bc4c83028621863c17202fa94a186e6
poimsm <poimsm@gmail.com>
2018-11-26 17:55:56 -0300

    CORS

--- server/app.js
+++ server/app.js
@@ -14,6 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 // habilitar carpeta public
 app.use( express.static( path.resolve(__dirname, '../public') ))
 
