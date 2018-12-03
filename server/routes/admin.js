bd700618fa174a358c00062574e61961d77f65ef
Rih <rodrigo.ediaz.f@gmail.com>
2018-12-02 20:36:19 -0400

    added store

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,19 +2,31 @@ const express = require('express');
 const router = require('express-promise-router')();
 const passport = require('passport');
 
-const applyController = require('../controllers/job-apply');
+const jobsController = require('../controllers/job-apply');
+const storeController = require('../controllers/store-apply');
 
 const passportJWT = passport.authenticate('jwt', { session: false });
 
 
 // -----------  Shoppers --------------------
 router.route('/jobs/apply')
-  .post(passportJWT, applyController.create);
+  .post(passportJWT, jobsController.create);
 
 router.route('/jobs/all')
-  .get(passportJWT, applyController.all);
+  .get(passportJWT, jobsController.all);
 
 router.route('/jobs/resolution/:id')
-  .post(passportJWT, applyController.resolution);
+  .post(passportJWT, jobsController.resolution);
+
+
+// -----------  Stores --------------------
+router.route('/store/apply')
+  .post(passportJWT, storeController.create);
+
+router.route('/store/all')
+  .get(passportJWT, storeController.all);
+
+router.route('/store/resolution/:id')
+  .post(passportJWT, storeController.resolution);
 
 module.exports = router;
